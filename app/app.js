b2afe6a54d1941804e43cbc90bc902ef7e990551
David Hayes <davidlynnhayes@att.net>
2015-11-02 10:55:48 -0600

    removed obsolete code from app.js

--- app/app.js
+++ app/app.js
@@ -45,22 +45,22 @@
 
       .otherwise({redirectTo: '/login'});
   }])
-  .run(['$rootScope', '$location', '$cookies', '$http',
-    function($rootScope, $location, $cookies, $http) {
-      // keep user logged in after page refresh
-      $rootScope.globals = $cookies.get('globals') || {};
-      if ($rootScope.globals.currentUser) {
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
-      }
-
-      $rootScope.$on('$locationChangeStart', function(event, next, current) {
-        // redirect to login page if not logged in
-        var openPages = ['/login', '/register'];
-        var restrictedPage = openPages.indexOf($location.path()) === -1;
-        var loggedIn = $rootScope.globals.currentUser;
-        var loggedIn = true;
-        if (restrictedPage && !loggedIn) {
-          $location.path('/login');
-        }
-      });
+  // .run(['$rootScope', '$location', '$http',
+  //   function($rootScope, $location, $http) {
+  //     // keep user logged in after page refresh
+  //     $rootScope.globals = $cookies.get('globals') || {};
+  //     if ($rootScope.globals.currentUser) {
+  //       $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+  //     }
+  //
+  //     $rootScope.$on('$locationChangeStart', function(event, next, current) {
+  //       // redirect to login page if not logged in
+  //       var openPages = ['/login', '/register'];
+  //       var restrictedPage = openPages.indexOf($location.path()) === -1;
+  //       var loggedIn = $rootScope.globals.currentUser;
+  //       var loggedIn = true;
+  //       if (restrictedPage && !loggedIn) {
+  //         $location.path('/login');
+  //       }
+  //     });
   }]);
